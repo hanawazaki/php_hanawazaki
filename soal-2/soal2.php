c6775f701f298e7822aa50286dd4cd4a55809680
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2025-04-13 11:35:50 +0700

    Update soal2.php

--- soal-2/soal2.php
+++ soal-2/soal2.php
@@ -12,12 +12,21 @@ if (!$conn) {
   die("konekso gagal" . mysqli_connct_error());
 }
 
-$query = "SELECT hobi.hobi as hobi,COUNT(hobi.hobi) AS jumlah_person
+if (isset($_GET['hobi'])) {
+  $hobi = $_GET['hobi'];
+  $query = "SELECT hobi.hobi as hobi,COUNT(hobi.hobi) AS jumlah_person 
+            FROM hobi 
+            JOIN person ON hobi.person_id = person.id
+            WHERE hobi like '%" . $hobi . "%'
+            GROUP BY hobi.hobi ;";
+} else {
+  $query = "SELECT hobi.hobi as hobi,COUNT(hobi.hobi) AS jumlah_person
 FROM hobi 
 JOIN person ON hobi.person_id = person.id
 
 GROUP BY hobi.hobi
 ;";
+}
 
 $result = mysqli_query($conn, $query);
 
@@ -36,14 +45,18 @@ $result = mysqli_query($conn, $query);
 
 <body>
   <div>
-    <input type="text" placeholder="search by hobi" name="hobi">
+    <form action="soal2.php" method="GET">
+      <input type="text" placeholder="search by hobi" name="hobi">
+      <input type="submit" value="search">
+      <button type="button" onclick="window.location.href='soal2.php'">Reset</button>
+    </form>
   </div>
   <table>
     <tr>
       <th>Hobi</th>
       <th>Jumlah person</th>
     </tr>
-    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+    <?php while ($row = mysqli_fetch_array($result)): ?>
       <tr>
         <td><?php echo $row['hobi']; ?></td>
         <td><?php echo $row['jumlah_person']; ?></td>
